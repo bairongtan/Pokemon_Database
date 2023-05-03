03199b8ac79da69855f924fd08d420fe7139de1f
bairongtan <108006784+bairongtan@users.noreply.github.com>
2023-05-03 15:17:07 -0400

    Added countDataClasses function to script

--- script.js
+++ script.js
@@ -2,23 +2,69 @@ async function getWebsiteBreachData (){
   const url = 'https://haveibeenpwned.com/api/v2/breaches';
   const data = await fetch (url);
   const bData = await data.json();
-  const storedBData = JSON.stringify(bData)
-  console.log(storedBData);
+  //const storedBData = JSON.stringify(bData)
+  //console.log(bData);
   console.log("loaded data")
+  countDataClasses(bData);
 }
 
+//async function getWebsiteBreachData (){
+//const loadDataButton = document.querySelector('#data_load');
 
+//loadDataButton.addEventListener('click', async (SubmitEvent) => {
+  //console.log("loaded data");
+  //const url = 'https://haveibeenpwned.com/api/v2/breaches';
+  //const data = await fetch (url);
+  //const bData = await data.json();
+  //countDataClasses(bData);
+//});
+
+function countDataClasses (bData){
+let countClasses = {
+  "emailAddresses" : 0,
+  "passwords": 0,
+  "phoneNumbers" : 0,
+  "ipAddresses" : 0,
+  "names": 0,
+  "physicalAddress": 0
+}
+bData.forEach(element => {
+  if (element.DataClasses.includes("Email addresses")){
+    countClasses.emailAddresses += 1
+  }
+  if (element.DataClasses.includes("Passwords")){
+    countClasses.passwords += 1
+  }
+  if (element.DataClasses.includes("Phone numbers")){
+    countClasses.phoneNumbers += 1
+  }
+  if (element.DataClasses.includes("IP addresses")){
+    countClasses.ipAddresses += 1
+  }
+  if (element.DataClasses.includes("Names")){
+    countClasses.names += 1
+  }
+  if (element.DataClasses.includes("Physical addresses")){
+    countClasses.physicalAddress += 1
+  }
+
+});
+  console.log(countClasses)
+  return countClasses;
+//return an array of numbers
+}
 
 const ctx = document.getElementById('myChart');
 
 new Chart(ctx, {
-type: 'scatter',
+type: 'bar',
 data: {
-  labels: ['Email Addresses', 'IP Addresses', 'Names', 'Passwords', 'Phone Numbers', 'Genders'],
+  labels: ['Email Addresses', 'Passwords', 'Phone Numbers', 'IP Addresses', 'Names', 'Physical Addresses'],
   datasets: [{
     label: '# of Votes',
-    data: [12, 19, 3, 5, 2, 3],
-    borderWidth: 1
+    data: countDataClasses(bData),
+    //data: [1,2,3,4,5],
+    borderWidth: 2
   }]
 },
 options: {
